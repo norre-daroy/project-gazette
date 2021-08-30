452d1da565989ef57680814d34120b50b04f8557
Norre <norrej.daroy@gmail.com>
2021-08-30 18:11:46 -0400

    feat: Add blog data

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
-import Head from "next/head";
-import Layout, { siteTitle } from "../components/layout";
-import utilStyles from "../styles/utils.module.css";
+import Head from 'next/head';
+import Link from 'next/link';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
 
 export default function Home() {
   return (
@@ -9,14 +10,20 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
+        <ul>
+          <li>About Us</li>
+          <li>Blog</li>
+          <li>Contact Us</li>
+        </ul>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
           aliquid dignissimos, optio perspiciatis pariatur vero molestiae
           voluptas quae atque aspernatur quisquam doloribus deleniti ipsum
           commodi minima sit. Quaerat, repellat est?
         </p>
+        <Link href="/posts/first-post">First Post</Link>
         <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
+          (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
